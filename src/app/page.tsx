78a819e034dfc0987343a329b979f0ae66a8932d
guwno-2137 <113305838+guwno-2137@users.noreply.github.com>
2026-02-01 20:50:51 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Roman Zupa Store 🔥</h1>
+      <p>Strona działa. Teraz podpinamy Firebase.</p>
+    </main>
+  );
+}
