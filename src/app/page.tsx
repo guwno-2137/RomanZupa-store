4007397903aa9cc1d1d7044f0a37d9b238fb08f2
guwno-2137 <113305838+guwno-2137@users.noreply.github.com>
2026-02-01 20:53:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-export default function Home() {
+import { getProducts } from "@/lib/products";
+
+export default async function Home() {
+  const products = await getProducts();
+
   return (
-    <main style={{ padding: 40 }}>
+    <main style={{ padding: 30 }}>
       <h1>Roman Zupa Store 🔥</h1>
-      <p>Strona działa. Teraz podpinamy Firebase.</p>
+
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2,1fr)",
+        gap: 20,
+        marginTop: 30
+      }}>
+        {products.map((p: any) => (
+          <div key={p.id} style={{ border: "1px solid #ddd", padding: 10 }}>
+            <img src={p.image} style={{ width: "100%" }} />
+            <h3>{p.name}</h3>
+            <p>{p.price} zł</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
